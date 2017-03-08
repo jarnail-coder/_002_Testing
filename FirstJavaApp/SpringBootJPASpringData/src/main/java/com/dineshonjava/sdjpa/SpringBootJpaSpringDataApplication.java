6d28d85e8a8457504c6091a4fce01c9bc081bca5
jarnail-coder <jarnailsinghcoder@gmail.com>
2017-03-08 17:28:28 +0530

    Update SpringBootJpaSpringDataApplication.java

--- FirstJavaApp/SpringBootJPASpringData/src/main/java/com/dineshonjava/sdjpa/SpringBootJpaSpringDataApplication.java
+++ FirstJavaApp/SpringBootJPASpringData/src/main/java/com/dineshonjava/sdjpa/SpringBootJpaSpringDataApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootJpaSpringDataApplication {
-//	Just For Testing Perpose
-//	Updated Again
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJpaSpringDataApplication.class, args);
 	}
