eeaf0e329d802b13ba7806d09dc69a9077464e62
Jarnail Singh <jarnailsingh45@gmail.com>
2017-03-08 17:33:28 +0530

    Commit Changes

--- FirstJavaApp/SpringBootJPASpringData/src/main/java/com/dineshonjava/sdjpa/SpringBootJpaSpringDataApplication.java
+++ FirstJavaApp/SpringBootJPASpringData/src/main/java/com/dineshonjava/sdjpa/SpringBootJpaSpringDataApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootJpaSpringDataApplication {
 //	Just For Testing Perpose
+//	You name is
 //	THis is Jarnail
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJpaSpringDataApplication.class, args);
